fc4fb3e96d3563416ca45b7ca18a587d3d4c41a1
RoshanS-TAMU <64324109+RoshanS-TAMU@users.noreply.github.com>
2023-03-30 23:42:40 -0500

    Update handshake.cpp

--- handshake.cpp
+++ handshake.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
+#include <semaphore.h>
+#include <thread>
+using namespace std;
 // include additional necessary headers
 
-void query(/*add necessary parameters*/) {
+void query(int printNum,sem_t responseSem,sem_t querySem) {
     // Should print: the print number (starting from 0), "SYN", and the three dots "..."
+    for (int i=0;i<printNum;++i){
+        if (i>0){sem_wait(&querySem);}
+    printf("[%d] SYN ...", i);
+    sem_post(&responseSem);
+    }
 }
 
-void response(/*add necessary parameters*/) {
+void response(int count,sem_t responseSem,sem_t querySem) {
     // Should print "ACK"
+    for (int i=0;i<count;++i){
+        if(i>0){sem_wait(&responseSem);}
+    printf("ACK\n");
+    sem_post(&querySem);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -18,11 +31,18 @@ int main(int argc, char** argv) {
     /**
      * Steps to follow:
      * 1. Get the "count" from cmd args
+     
      * 2. Create necessary synchronization primitive(s)
      * 3. Create two threads, one for "SYN" and the other for "ACK"
      * 4. Provide the threads with necessary args
      * 5. Update the "query" and "response" functions to synchronize the output
     */
    
+   int count = atoi(argv[1]);
+    sem_t querySem, responseSem;
+    thread t1(query,count,responseSem,querySem);
+    thread t2(response,count,responseSem,querySem);
+    t1.join();
+    t2.join();
     return 0;
 }
